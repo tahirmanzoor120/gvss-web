11be4610f7de5e7a1f2e48b9b2999542fb26b950
Muhammad Tahir <tahirmanzoor120@gmail.com>
2026-05-08 03:26:43 +0500

    Added missing keys and parking mode

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,11 @@ export default defineConfig(() => ({
     proxy: {
       // '/api/socket': 'ws://localhost:8082',
       // '/api': 'http://localhost:8082',
-      '/api/socket': 'wss://live.gvss.pk',
-      '/api': 'https://live.gvss.pk',
+      // '/api/socket': 'wss://live.gvss.pk',
+      // '/api': 'https://live.gvss.pk',
+      '/api/socket': 'ws://178.128.152.90:8082',
+      '/api': 'http://178.128.152.90:8082',
+
     },
   },
   build: {
